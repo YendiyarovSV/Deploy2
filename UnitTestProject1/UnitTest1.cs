bd87c7684752f66751c868ca24b206843a198628
Yendiyarov Sergei <megoplay20@gmail.com>
2018-02-27 22:00:36 +0600

    aded tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, 2);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(3, 3);
+
+        }
+
+        [TestMethod]
+        public void TestFail()
+        {
+            Assert.AreEqual(3, 2);
+
+        }
+
+
+
+    }
+}
